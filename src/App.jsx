4af74aec896af23f2f9ef3d07d2f56a058700c1a
Imre Kiss <jakab.asd@gmail.com>
2019-09-19 16:49:17 +0200

    Config eslint ann fix the beginning issues

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Series Rush</p>
+    </header>
+  </div>
+);
+
+export default App;
