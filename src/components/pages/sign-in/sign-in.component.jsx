8d254c5828e9b504b1aaf27055f4182e1e5e6d7c
Imre Kiss <jakab.asd@gmail.com>
2019-09-20 19:33:06 +0200

    Add lock icon to the sign-in page

--- src/components/pages/sign-in/sign-in.component.jsx
+++ src/components/pages/sign-in/sign-in.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+
+import LockIcon from '@material-ui/icons/LockOutlined';
 
 import SignInForm from './sign-in-form/sign-in-form.container';
 
-const SignIn = () => (
-  <Container maxWidth="xs">
-    <SignInForm />
-  </Container>
-);
+import useStyles from './sign-in.styles';
+
+const SignIn = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="xs">
+      <Box className={classes.avatarBox}>
+        <Avatar className={classes.avatar}>
+          <LockIcon />
+        </Avatar>
+      </Box>
+
+      <SignInForm />
+    </Container>
+  );
+};
 
 export default SignIn;
