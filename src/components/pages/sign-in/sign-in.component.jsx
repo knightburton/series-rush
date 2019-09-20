84ca90dc827108118acbb83fee762fc1723162aa
Imre Kiss <jakab.asd@gmail.com>
2019-09-20 13:20:57 +0200

    Add a Landing and Sign-In page shells to app routes

---
+++ src/components/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+
+const SignIn = () => (
+  <Container maxWidth={false}>
+    Sign-In
+  </Container>
+);
+
+export default SignIn;
--- src/routes/app-routes.jsx
+++ src/routes/app-routes.jsx
@@ -1,6 +1,15 @@
-// import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-const AppRoutes = () => null;
+import Landing from '../components/pages/landing/landing.container';
+import SignIn from '../components/pages/sign-in/sign-in.container';
+
+const AppRoutes = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route exact path="/sign-in" component={SignIn} />
+    <Redirect to="/" />
+  </Switch>
+);
 
 export default AppRoutes;
