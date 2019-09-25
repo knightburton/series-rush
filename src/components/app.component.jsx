98d4346e4f5de5187a84de19b530b1a9c1ed9f14
Imre Kiss <jakab.asd@gmail.com>
2019-09-25 15:03:43 +0200

    Add waiting state to the app while loading the firebase auth

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AppBar from './widgets/appbar/appbar.container';
+import Waiting from './widgets/waiting/waiting.component';
 
 import { ProfileProvider } from './contexts/profile';
 
 import AppRoutes from '../routes/app-routes';
 
-const App = () => (
+const App = ({ authIsLoaded }) => (authIsLoaded ? (
   <>
     <ProfileProvider>
       <AppBar />
       <AppRoutes />
     </ProfileProvider>
   </>
-);
+) : (
+  <Waiting />
+));
+
+App.propTypes = {
+  authIsLoaded: PropTypes.bool.isRequired,
+};
 
 export default App;
