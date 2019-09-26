af347916b6530c6762f5ff94141e752f5f89907d
Imre Kiss <jakab.asd@gmail.com>
2019-09-26 11:30:26 +0200

    Add profile page shell

---
+++ src/components/pages/profile/profile.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+
+const Profile = () => (
+  <Container maxWidth="md">
+    Profile
+  </Container>
+);
+
+export default Profile;
--- src/routes/app-routes.jsx
+++ src/routes/app-routes.jsx
@@ -5,6 +5,7 @@ import Landing from '../components/pages/landing/landing.container';
 import SignIn from '../components/pages/sign-in/sign-in.container';
 import SignUp from '../components/pages/sign-up/sign-up.container';
 import ForgotPassword from '../components/pages/forgot-password/forgot-password.container';
+import Profile from '../components/pages/profile/profile.container';
 
 const AppRoutes = () => (
   <Switch>
@@ -12,6 +13,7 @@ const AppRoutes = () => (
     <Route exact path="/sign-in" component={SignIn} />
     <Route exact path="/sign-up" component={SignUp} />
     <Route exact path="/forgot-password" component={ForgotPassword} />
+    <Route exact path="/profile" component={Profile} />
     <Redirect to="/" />
   </Switch>
 );
