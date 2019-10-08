4ddf353e18f9cd6517b9fb0f483e408fb3d6afa0
Imre Kiss <jakab.asd@gmail.com>
2019-10-09 00:25:23 +0200

    Move the quick search result component content into the search page

--- src/components/pages/search/search.container.js
+++ src/components/pages/search/search.container.js
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux';
 
+import { getSerachResult } from '../../../store/search';
 import Search from './search.component';
 
-const mapStateToProps = () => ({});
+const mapStateToProps = state => ({
+  result: getSerachResult(state),
+});
 
 const mapDispatchToProps = {};
 
