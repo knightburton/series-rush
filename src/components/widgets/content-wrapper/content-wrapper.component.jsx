950e4aca4055b4b354b68d412a9ab5ccd3d66cd2
Imre Kiss <jakab.asd@gmail.com>
2019-10-09 16:24:04 +0200

    Hide the search bar in case of signed out profile

--- src/components/widgets/content-wrapper/content-wrapper.component.jsx
+++ src/components/widgets/content-wrapper/content-wrapper.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import Box from '@material-ui/core/Box';
@@ -7,20 +7,27 @@ import Divider from '@material-ui/core/Divider';
 
 import QuickSearch from '../quick-search/quick-search.container';
 
+import ProfileContext from '../../../contexts/profile';
+
 import useStyles from './content-wrapper.styles';
 
 const ContentWrapper = ({ children }) => {
   const classes = useStyles();
+  const { signedIn } = useContext(ProfileContext);
 
   return (
     <Box className={classes.base}>
       <Box className={classes.toolbar} />
-      <Box className={classes.searchWrapper}>
-        <Container maxWidth="lg">
-          <QuickSearch />
-        </Container>
-      </Box>
-      <Divider />
+      {signedIn && (
+        <>
+          <Box className={classes.searchWrapper}>
+            <Container maxWidth="lg">
+              <QuickSearch />
+            </Container>
+          </Box>
+          <Divider />
+        </>
+      )}
       <Box className={classes.content}>
         {children}
       </Box>
--- src/components/widgets/content-wrapper/content-wrapper.container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import ContentWrapper from './content-wrapper.component';
-
-const mapStateToProps = () => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContentWrapper);
