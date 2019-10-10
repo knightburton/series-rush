47579aba880db580c68cd8390d69f16ffef21833
Imre Kiss <jakab.asd@gmail.com>
2019-10-10 14:21:07 +0200

    Remove the legacy parts of the search result component

--- src/components/pages/search/search.component.jsx
+++ src/components/pages/search/search.component.jsx
@@ -4,24 +4,17 @@ import { useTranslation } from 'react-i18next';
 
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Rating from '@material-ui/lab/Rating';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
 
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
 
-import AddIcon from '@material-ui/icons/AddOutlined';
-import InfoIcon from '@material-ui/icons/InfoOutlined';
-
-import Tooltip from '../../commons/tooltip/tooltip.component';
+import AddIcon from '@material-ui/icons/AddCircleOutline';
+import PublicIcon from '@material-ui/icons/PublicOutlined';
 
 import useStyles from './search.styles';
 
@@ -36,7 +29,7 @@ const Search = ({ result }) => {
           {result.map(({ show }) => (
             <Grid item key={show.id} xs={12} md={6}>
               <Card>
-                <Grid container>
+                <Grid container spacing={0}>
                   <Grid item xs={4}>
                     <img
                       src={show.image ? show.image.medium : 'https://via.placeholder.com/70x98?text=...'}
@@ -46,75 +39,34 @@ const Search = ({ result }) => {
                     />
                   </Grid>
                   <Grid item xs={8}>
+                    <CardHeader title={show.name} />
                     <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {show.name}
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {`${t('page.search.premiered')}: ${show.premiered || t('common:unknown')}`}
+                      </Typography>
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {`${t('page.search.status')}: ${show.status || t('common:unknown')}`}
+                      </Typography>
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {`${t('page.search.rating')}: ${(show.rating && show.rating.average) || t('common:unknown')}`}
                       </Typography>
                     </CardContent>
+                    <CardActions disableSpacing>
+                      <IconButton
+                        href={show.officialSite || ''}
+                        component={Link}
+                        target="_blank"
+                        disabled={!show.officialSite}
+                      >
+                        <PublicIcon />
+                      </IconButton>
+                      <IconButton className={classes.arButton}>
+                        <AddIcon />
+                      </IconButton>
+                    </CardActions>
                   </Grid>
                 </Grid>
               </Card>
-
-              {/* <Paper>
-                <Grid container className={classes.grid}>
-                  <Grid item xs={3}>
-                    <img
-                      src={show.image ? show.image.medium : 'https://via.placeholder.com/70x98?text=...'}
-                      alt={show.name}
-                      className={classes.image}
-                      draggable={false}
-                    />
-                  </Grid>
-                  <Grid
-                    item
-                    container
-                    xs={9}
-                    direction="column"
-                    justify="space-between"
-                    className={classes.innerGrid}
-                  >
-                    <Box>
-                      <Typography variant="h6">
-                        {show.name}
-                      </Typography>
-                      <Rating
-                        name={`rating-for${show.id}`}
-                        value={show.rating ? Math.floor(show.rating.average) : 0}
-                        max={10}
-                        size="small"
-                        readOnly
-                        disabled
-                      />
-                    </Box>
-                    <Box className={classes.actionContainer}>
-                      <Link
-                        href={show.officialSite || ''}
-                        component={Button}
-                        target="_blank"
-                        disabled={!show.officialSite}
-                        className={classes.officialSite}
-                        disableRipple
-                        disableFocusRipple
-                        disableTouchRipple
-                      >
-                        {t('quickSearch.officialSite')}
-                      </Link>
-                      <Box>
-                        <Tooltip title={t('quickSearch.add')}>
-                          <IconButton color="primary">
-                            <AddIcon fontSize="small" />
-                          </IconButton>
-                        </Tooltip>
-                        <Tooltip title={t('quickSearch.showInfo')}>
-                          <IconButton color="secondary">
-                            <InfoIcon fontSize="small" />
-                          </IconButton>
-                        </Tooltip>
-                      </Box>
-                    </Box>
-                  </Grid>
-                </Grid>
-              </Paper> */}
             </Grid>
           ))}
         </Grid>
--- src/components/pages/search/search.styles.js
+++ src/components/pages/search/search.styles.js
@@ -8,28 +8,7 @@ export default makeStyles(theme => ({
     width: '100%',
     height: '100%',
   },
-  innerGrid: {
-    paddingLeft: theme.spacing(1),
-  },
-  image: {
-    width: '100%',
-    height: 'auto',
-    borderRadius: theme.shape.borderRadius,
-    borderWidth: 1,
-    borderColor: theme.palette.grey.A100,
-    borderStyle: 'solid',
-  },
-  actionContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: theme.spacing(0.5, 0),
-  },
-  officialSite: {
-    textTransform: 'none',
-    padding: 0,
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
+  arButton: {
+    marginLeft: 'auto',
   },
 }));
