398f595877d18822ac815115de827ede1595b89a
Imre Kiss <jakab.asd@gmail.com>
2019-10-11 12:42:47 +0200

    Add a footer component and restructure the base layout theme

--- src/components/widgets/content-wrapper/content-wrapper.styles.js
+++ src/components/widgets/content-wrapper/content-wrapper.styles.js
@@ -8,12 +8,13 @@ export default makeStyles(theme => ({
     position: 'fixed',
     width: '100%',
     backgroundColor: theme.palette.background.paper,
+    zIndex: theme.zIndex.appBar - 1,
   },
   content: {
     padding: theme.spacing(3, 0),
   },
   padding: {
-    paddingTop: theme.spacing(7),
+    paddingTop: theme.spacing(6),
   },
   toolbar: {
     ...theme.mixins.toolbar,
