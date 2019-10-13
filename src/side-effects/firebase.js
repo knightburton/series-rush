b3ae63555d87156bf8d96d497ebb0d0ee86407b1
Imre Kiss <jakab.asd@gmail.com>
2019-10-13 16:11:43 +0200

    Change all the project credentials and do not show them in public

--- src/side-effects/firebase.js
+++ src/side-effects/firebase.js
@@ -5,13 +5,14 @@ import 'firebase/storage';
 import 'firebase/auth';
 
 export const fConfig = {
-  apiKey: 'AIzaSyDSiHSZ4_hwYgaJOdts2_fBd5nk-e_lNXM',
-  authDomain: 'series-rush.firebaseapp.com',
-  databaseURL: 'https://series-rush.firebaseio.com',
-  projectId: 'series-rush',
-  storageBucket: 'series-rush.appspot.com',
-  messagingSenderId: '682132416209',
-  appId: '1:682132416209:web:1d67e168993cb6f89947f0',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export const rfConfig = {
