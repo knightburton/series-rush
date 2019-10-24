29179ea5b203e9f3927cd62fc8f70f0c16d9fbf8
Imre Kiss <jakab.asd@gmail.com>
2019-10-24 20:40:02 +0200

    Add app store reducer unit tests #25

--- src/test/store/app/index.spec.js
+++ src/test/store/app/index.spec.js
@@ -1,5 +1,7 @@
 import * as app from '../../../store/app';
 
+const utils = require('../../../utils');
+
 // Mock data
 const mockTimestamp = 1571841496157;
 const mockAlert = {
@@ -101,5 +103,174 @@ describe('App Selectors', () => {
 
   test('getTmdbConfiguration', () => {
     expect(app.getTmdbConfiguration(state)).toEqual(mockState.tmdbConfiguration);
+  });
+});
+
+// Reducer unit tests
+describe('App reducer', () => {
+  let state = app.initialState;
+
+  beforeAll(() => {
+    utils.getTimestamp = jest.fn(() => mockTimestamp);
+  });
+
+  beforeEach(() => {
+    state = app.initialState;
+  });
+
+  afterAll(() => {
+    jest.unmock('../../../utils');
+  });
+
+  test('Initial State', () => {
+    expect(state).toEqual(app.initialState);
+  });
+
+  test(`${app.ADD_ALERT} (full version)`, () => {
+    const expected = expect(
+      app.reducer(state, {
+        type: app.ADD_ALERT,
+        payload: {
+          message: mockAlert.message,
+          variant: mockAlert.variant,
+        },
+      })
+    );
+    expected.toHaveProperty('alerts', [mockAlert]);
+  });
+
+  test(`${app.ADD_ALERT} (only message -> variant = 'info')`, () => {
+    const expected = expect(
+      app.reducer(state, {
+        type: app.ADD_ALERT,
+        payload: {
+          message: mockAlert.message,
+        },
+      })
+    );
+    expected.toHaveProperty('alerts', [{
+      ...mockAlert,
+      variant: 'info',
+    }]);
+  });
+
+  test(`${app.REMOVE_ALERT} (with valid key)`, () => {
+    const expected = expect(
+      app.reducer(mockState, {
+        type: app.REMOVE_ALERT,
+        payload: mockAlert.key,
+      })
+    );
+    expected.toHaveProperty('alerts', []);
+  });
+
+  test(`${app.REMOVE_ALERT} (with invalid key)`, () => {
+    const expected = expect(
+      app.reducer(mockState, {
+        type: app.REMOVE_ALERT,
+        payload: mockAlert.key + 1,
+      })
+    );
+    expected.toHaveProperty('alerts', [mockAlert]);
+  });
+
+  test(`${app.SET_APP_WAITING} (0 -> 1)`, () => {
+    const expected = expect(
+      app.reducer(state, {
+        type: app.SET_APP_WAITING,
+        payload: true,
+      })
+    );
+    expected.toHaveProperty('waiting', 1);
+  });
+
+  test(`${app.SET_APP_WAITING} (1 -> 2)`, () => {
+    const localState = {
+      ...state,
+      waiting: 1,
+    };
+    const expected = expect(
+      app.reducer(localState, {
+        type: app.SET_APP_WAITING,
+        payload: true,
+      })
+    );
+    expected.toHaveProperty('waiting', 2);
+  });
+
+  test(`${app.SET_APP_WAITING} (1 -> 0)`, () => {
+    const localState = {
+      ...state,
+      waiting: 1,
+    };
+    const expected = expect(
+      app.reducer(localState, {
+        type: app.SET_APP_WAITING,
+        payload: false,
+      })
+    );
+    expected.toHaveProperty('waiting', 0);
+  });
+
+  test(`${app.SET_APP_WAITING} (0 -> -1)`, () => {
+    const expected = expect(
+      app.reducer(state, {
+        type: app.SET_APP_WAITING,
+        payload: false,
+      })
+    );
+    expected.toHaveProperty('waiting', -1);
+  });
+
+  test(`${app.TOGGLE_MOBILE_DRAWER} (false -> true)`, () => {
+    const expected = expect(
+      app.reducer(state, {
+        type: app.TOGGLE_MOBILE_DRAWER,
+      })
+    );
+    expected.toHaveProperty('isMobileDrawerOpened', true);
+  });
+
+  test(`${app.TOGGLE_MOBILE_DRAWER} (true -> false)`, () => {
+    const localState = {
+      ...state,
+      isMobileDrawerOpened: true,
+    };
+    const expected = expect(
+      app.reducer(localState, {
+        type: app.TOGGLE_MOBILE_DRAWER,
+      })
+    );
+    expected.toHaveProperty('isMobileDrawerOpened', false);
+  });
+
+  test(`${app.TMDB_CONFIGURATON_FINISH} ({})`, () => {
+    const expected = expect(
+      app.reducer(mockState, {
+        type: app.TMDB_CONFIGURATON_FINISH,
+        payload: {},
+      })
+    );
+    expected.toHaveProperty('tmdbConfiguration', {});
+  });
+
+  test(`${app.TMDB_CONFIGURATON_FINISH} (null)`, () => {
+    const expected = expect(
+      app.reducer(mockState, {
+        type: app.TMDB_CONFIGURATON_FINISH,
+        payload: null,
+      })
+    );
+    expected.toHaveProperty('tmdbConfiguration', null);
+  });
+
+  test(`${app.TMDB_CONFIGURATON_FINISH} (valid data)`, () => {
+    const expected = expect(
+      app.reducer(mockState, {
+        type: app.TMDB_CONFIGURATON_FINISH,
+        payload: mockTmdbConfiguration,
+      })
+    );
+    expected.toHaveProperty('tmdbConfiguration', mockTmdbConfiguration);
   });
 });
