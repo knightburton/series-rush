307f3d58bd5be356236cf855559580a65fd8f613
Imre Kiss <jakab.asd@gmail.com>
2019-11-13 22:11:43 +0100

    Adjust the project title to be aligned the drawer items

--- src/components/commons/project-title/project-title.styles.js
+++ src/components/commons/project-title/project-title.styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
   logo: {
-    marginRight: theme.spacing(3),
-    maxHeight: theme.spacing(6),
+    marginRight: theme.spacing(2),
+    maxHeight: theme.spacing(5),
   },
 }));
