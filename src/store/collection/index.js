2033743dd0a06457fb7c6c7fe0f51b05459c3053
Imre Kiss <jakab.asd@gmail.com>
2019-11-18 22:48:07 +0100

    Add a single collection group getter async action to collection store #37

--- src/store/collection/index.js
+++ src/store/collection/index.js
@@ -1,6 +1,9 @@
 import { handleActions } from 'redux-actions';
+import { createSelector } from 'reselect';
 import { setAppWaiting, addAlert } from '../app';
 import { getProfile } from '../auth';
+import { getFirestoreOrderedByCollection } from '../firestore';
+import { SEARCH_TYPES } from '../../constants/config';
 
 // Initial state
 export const initialState = {};
@@ -11,7 +14,34 @@ export const reducer = handleActions(
   initialState,
 );
 
+// Selectors
+export const getTVGroups = createSelector(
+  getFirestoreOrderedByCollection('groups'),
+  groups => groups && groups.length && groups.find(group => group.type === SEARCH_TYPES.TV),
+);
+export const getMovieGroups = createSelector(
+  getFirestoreOrderedByCollection('movie'),
+  groups => groups && groups.length && groups.find(group => group.type === SEARCH_TYPES.MOVIE),
+);
+
 // Async actions
+export const fetchCollectionGroups = () => async (dispatch, getState, { getFirestore }) => {
+  try {
+    const firestore = getFirestore();
+    const { id } = getProfile(getState());
+    await firestore.get({
+      collection: 'collections',
+      doc: id,
+      subcollections: [
+        { collection: 'groups' },
+      ],
+      storeAs: 'groups',
+    });
+  } catch (error) {
+    dispatch(addAlert('alert:collection/fetch-collection-group-failure', 'error'));
+  }
+};
+
 export const addToCollection = (id, type) => async (dispatch, getState, { getFirestore }) => {
   dispatch(setAppWaiting(true));
   try {
