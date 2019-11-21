669c1b6001a7febc872c87ad0f48a6d3824c3008
Imre Kiss <jakab.asd@gmail.com>
2019-11-21 15:36:30 +0100

    Display the available valid groups in movie collections as well #37

--- src/components/pages/collection/collection-movie/collection-movie.component.jsx
+++ src/components/pages/collection/collection-movie/collection-movie.component.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Box from '@material-ui/core/Box';
 
-const CollectionMovie = () => (
+import ChipArray from '../../../commons/chip-array/chip-array.component';
+
+const CollectionMovie = ({ groups }) => (
   <Box>
-    Collection Movie
+    <ChipArray
+      items={groups}
+      breakpoint="xs"
+    />
   </Box>
 );
+
+CollectionMovie.propTypes = {
+  groups: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string,
+    label: PropTypes.string,
+  })).isRequired,
+};
 
 export default CollectionMovie;
