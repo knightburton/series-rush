add0dc5aedf73b0ff2e8645f074c2e3b06e9b6a0
Imre Kiss <jakab.asd@gmail.com>
2019-12-06 16:07:44 +0100

    Add a group selector action into the collection store #37

--- src/store/collection/index.js
+++ src/store/collection/index.js
@@ -1,23 +1,50 @@
-import { handleActions } from 'redux-actions';
+import { handleActions, createAction } from 'redux-actions';
 import { createSelector } from 'reselect';
 import { setAppWaiting, addAlert } from '../app';
 import { getProfile } from '../auth';
 import { getFirestoreOrderedByPath } from '../firestore';
 
 // Initial state
-export const initialState = {};
+export const initialState = {
+  selectedGroup: {
+    tv: null,
+    movie: null,
+  },
+};
+
+// Action types
+export const COLLECTION_SELECT_GROUP = 'COLLECTION_SELECT_GROUP';
+
+// Actions
+export const collectionSelectGroup = createAction(
+  COLLECTION_SELECT_GROUP,
+  (type, group) => ({ type, group }),
+);
 
 // Reducer
 export const reducer = handleActions(
-  {},
+  {
+    [collectionSelectGroup]: (state, { payload: { type, group } }) => ({
+      ...state,
+      selectedGroup: {
+        ...state.selectedGroup,
+        [type]: group,
+      },
+    }),
+  },
   initialState,
 );
 
 // Selectors
+export const getSelectedGroup = state => state.collection.selectedGroup;
+export const getSelectedGroupTv = state => state.collection.selectedGroup.tv;
+export const getSelectedGroupMovie = state => state.collection.selectedGroup.movie;
+
 export const getGroupsByType = type => createSelector(
   getFirestoreOrderedByPath(`${type}Groups`),
   groups => groups.sort((a, b) => a.order - b.order),
 );
+
 export const getCollectionByType = type => createSelector(
   getFirestoreOrderedByPath(`${type}Collection`),
   collection => collection,
@@ -27,7 +54,7 @@ export const getCollectionByTypeAndGroup = (type, group) => createSelector(
   collection => collection.filter(item => item.group === group),
 );
 
-// Async actions
+// Thunks
 export const addToCollection = (id, type, group) => async (dispatch, getState, { getFirestore }) => {
   dispatch(setAppWaiting(true));
   try {
