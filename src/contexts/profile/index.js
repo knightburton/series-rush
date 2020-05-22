4822692beddcda6af2cbae4a54310399c0b3fb8a
Imre Kiss <imre.kiss@jaystack.com>
2020-05-22 16:39:51 +0200

    Use the redux hooks inside the profile context as well #62

--- src/contexts/profile/index.js
+++ src/contexts/profile/index.js
@@ -1,18 +1,6 @@
-import { connect } from 'react-redux';
-import {
-  getProfile,
-  getFirebaseAuthIsLoaded,
-} from '../../store/auth';
 import ProfileProviderComponent from './profile.provider';
 import ProfileContext from './context';
 
-const mapStateToProps = state => ({
-  authIsLoaded: getFirebaseAuthIsLoaded(state),
-  profile: getProfile(state),
-});
-
-const mapDispatchToProps = {};
-
-export const ProfileProvider = connect(mapStateToProps, mapDispatchToProps)(ProfileProviderComponent);
+export const ProfileProvider = ProfileProviderComponent;
 export const ProfileConsumer = ProfileContext.Consumer;
 export default ProfileContext;
