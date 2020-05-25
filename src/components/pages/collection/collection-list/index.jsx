b0545e1c56f6e7a4021621c7b0fb9d4af60a1573
Imre Kiss <jakab.asd@gmail.com>
2020-05-25 20:19:06 +0200

    start to prepare the collection page for the groups edit #37

--- src/components/pages/collection/collection-list/index.jsx
+++ src/components/pages/collection/collection-list/index.jsx
@@ -1,14 +1,18 @@
 import React, { useCallback } from 'react';
 import { useParams } from 'react-router';
+import { useTranslation } from 'react-i18next';
 import {
   useDispatch,
   useSelector,
 } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+
+import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
 
 import ChipArray from '../../../commons/chip-array';
-
+import Tooltip from '../../../commons/tooltip';
 import CollectionListItem from './collection-list-item';
 
 import {
@@ -19,8 +23,9 @@ import {
 } from '../../../../store/collection';
 
 const CollectionList = () => {
-  const dispatch = useDispatch();
+  const { t } = useTranslation();
   const { type } = useParams();
+  const dispatch = useDispatch();
   const groups = useSelector(getGroupsByType(type));
   const selectedGroup = useSelector(getSelectedGroupByType(type));
   const list = useSelector(getCollectionByTypeAndGroup(type, selectedGroup));
@@ -31,12 +36,28 @@ const CollectionList = () => {
 
   return (
     <Box>
-      <ChipArray
-        items={groups}
-        breakpoint="xs"
-        selected={selectedGroup}
-        onClick={handleChipClick}
-      />
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignContent="center"
+        alignItems="center"
+        mb={1}
+      >
+        <ChipArray
+          items={groups}
+          breakpoint="xs"
+          selected={selectedGroup}
+          onClick={handleChipClick}
+        />
+        <Tooltip title={t('page.collection.editGroups')}>
+          <IconButton>
+            <EditTwoToneIcon
+              fontSize="small"
+              color="secondary"
+            />
+          </IconButton>
+        </Tooltip>
+      </Box>
       {list.map(item => (
         <CollectionListItem
           key={item.id}
