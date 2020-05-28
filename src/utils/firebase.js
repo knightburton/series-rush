0156334041e5506942987af9840e0f8373ce627e
Imre Kiss <imre.kiss@jaystack.com>
2020-05-28 15:56:24 +0200

    Change the group listener in the firebase utils to get back the groups in order #37

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -28,6 +28,7 @@ export const getProfileGroupsByTypeQuery = (profileID, type) => ({
   subcollections: [{
     collection: 'groups',
     where: ['type', '==', type],
+    orderBy: ['order', 'asc'],
   }],
   storeAs: `${type}Groups`,
 });
