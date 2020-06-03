de22c16076ab935df1c20b60f10f00215ce07c44
Imre Kiss <jakab.asd@gmail.com>
2020-06-03 20:27:43 +0200

    Add a go back button to the collection groups edit page header #37

--- src/components/pages/collections/collections-edit/collections-edit-groups/collections-edit-groups-header/index.jsx
+++ src/components/pages/collections/collections-edit/collections-edit-groups/collections-edit-groups-header/index.jsx
@@ -1,6 +1,9 @@
 import React, { useCallback } from 'react';
-import { useParams } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import {
+  useParams,
+  useHistory,
+} from 'react-router';
 import {
   useDispatch,
   useSelector,
@@ -10,6 +13,7 @@ import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
 
 import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
+import ArrowBackTwoToneIcon from '@material-ui/icons/ArrowBackTwoTone';
 
 import PageTitle from '../../../../../commons/page-title';
 import Tooltip from '../../../../../commons/tooltip';
@@ -19,10 +23,13 @@ import {
   openCollectionsDialog,
   setCollectionsDialogData,
 } from '../../../../../../store/collections';
+import { APP_PATHS } from '../../../../../../constants/paths';
+import { SEARCH_TYPES } from '../../../../../../constants/config';
 
 const CollectionsEditGroupsHeader = () => {
   const { t } = useTranslation();
   const { type } = useParams();
+  const { push } = useHistory();
   const dispatch = useDispatch();
   const isGroupAddEnabled = useSelector(getIsGroupAddEnabled(type));
 
@@ -31,6 +38,13 @@ const CollectionsEditGroupsHeader = () => {
     dispatch(openCollectionsDialog('form'));
   }, [dispatch]);
 
+  const handleGoBackClick = useCallback(() => {
+    const route = type === SEARCH_TYPES.TV
+      ? APP_PATHS.COLLECTIONS_TV.path
+      : APP_PATHS.COLLECTIONS_MOVIE.path;
+    push(route);
+  }, [push, type]);
+
   return (
     <Box
       display="flex"
@@ -38,8 +52,17 @@ const CollectionsEditGroupsHeader = () => {
       alignContent="center"
       alignItems="center"
     >
-      <Box pt={1}>
-        <PageTitle title={t('page.collections.edit.groups.typeTitle', { type })} />
+      <Box display="flex">
+        <Box mr={1}>
+          <Tooltip title={t('common::goBack')}>
+            <IconButton onClick={handleGoBackClick}>
+              <ArrowBackTwoToneIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Box pt={1}>
+          <PageTitle title={t('page.collections.edit.groups.typeTitle', { type })} />
+        </Box>
       </Box>
       <Box
         display="flex"
