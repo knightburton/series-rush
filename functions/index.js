7733fac7de6b82913c6604ce79f8627ba4a5ddb1
Imre Kiss <jakab.asd@gmail.com>
2020-06-25 15:06:18 +0200

    Implement a base collection item on create trigger for future usage #22

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const { userCleanup, userCreate } = require('./src/triggers/auth');
+// const { onProfileCollectionItemCreate } = require('./src/triggers/firestore');
 
 // Auth triggers
 exports.userCleanup = userCleanup;
 exports.userCreate = userCreate;
+
+// Firestore triggers
+// exports.onProfileCollectionItemCreate = onProfileCollectionItemCreate;
--- functions/src/triggers/firestore.js
+++ functions/src/triggers/firestore.js
@@ -1,7 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const tmdbApi = require('../request/tmdbApi');
 
-const onProfileCollectionItemCreate = functions.firestore.document('profiles/{userID}/collection/{itemID}').onCreate((snapshot, context) => {
+const { ITEM_TYPES } = require('../constants');
+const { COMMON_ERRORS } = require('../constants/errors');
 
+const onProfileCollectionItemCreate = functions.firestore.document('profiles/{userID}/collection/{itemID}').onCreate(async (snapshot, context) => {
+  const created = snapshot.data();
+
+  // Get the type of the created item.
+  const { type } = created;
+
+  try {
+    // Make sure we have a proper type and itemID.
+    if (!type || !Object.values(ITEM_TYPES).includes(type) || !context.params.itemID) throw new Error(COMMON_ERRORS.MISSING_PARAMS);
+
+    // Request the detials of the new item.
+    const detailedItem = await tmdbApi.getDetailsByTypeAndId(type, context.params.itemID);
+
+    // Store all the data right away as cache.
+    const cacheRef = admin.firestore().collection('cache').doc(context.params.itemID);
+    await cacheRef.set(detailedItem);
+
+    console.log('[onProfileCollectionItemCreate]: new item has been cached: ', context.params.itemID);
+  } catch (error) {
+    console.error('[onProfileCollectionItemCreate]: Failed with error: ', error);
+  }
 });
 
 module.exports = {
