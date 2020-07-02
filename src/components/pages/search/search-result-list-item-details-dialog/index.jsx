f9aff48044485243f33d5fd06fabce7bc83fcbc8
Imre Kiss <jakab.asd@gmail.com>
2020-07-02 15:05:41 +0200

    Display small base informations about tv shows on the SearchResultListItemDetailsDialog #71

--- src/components/pages/search/search-result-list-item-details-dialog/index.jsx
+++ src/components/pages/search/search-result-list-item-details-dialog/index.jsx
@@ -71,94 +71,128 @@ const SearchResultListItemDetailsDialog = () => {
       </DialogTitle>
       <DialogContent>
         <Grid container spacing={2}>
-          <Grid item xs="auto">
-            <Hidden xsDown>
-              <img
-                src={details?.posterPath}
-                alt={details?.name}
-                draggable={false}
-                className={classes.poster}
-              />
-            </Hidden>
-            <Hidden smUp>
-              <img
-                src={details?.backdropPath}
-                alt={details?.name}
-                draggable={false}
-                className={classes.backdrop}
-              />
-            </Hidden>
-          </Grid>
-          <Grid item>
-            <Box>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`${t('page.search.item.premiere')}: `}
-              </Typography>
-              <Typography>
-                {details?.premiere || t('common::unknown')}
-              </Typography>
-            </Box>
-            {PARSABLE_ITEM_TYPES.includes(details?.type) && (
-              <Box mt={1}>
+          <Grid container item spacing={2} xs={12}>
+            <Grid item xs="auto">
+              <Hidden xsDown>
+                <img
+                  src={details?.posterPath}
+                  alt={details?.name}
+                  draggable={false}
+                  className={classes.poster}
+                />
+              </Hidden>
+              <Hidden smUp>
+                <img
+                  src={details?.backdropPath}
+                  alt={details?.name}
+                  draggable={false}
+                  className={classes.backdrop}
+                />
+              </Hidden>
+            </Grid>
+            <Grid item>
+              <Box>
                 <Typography color="textSecondary" variant="subtitle2">
-                  {`${t(`page.search.item.${details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy'}`)}: `}
+                  {`${t('page.search.item.premiere')}: `}
                 </Typography>
                 <Typography>
-                  {details?.[details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy']?.join(', ') || t('common::unknown')}
+                  {details?.premiere || t('common::unknown')}
                 </Typography>
               </Box>
-            )}
-            <Box mt={1}>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`${t('page.search.item.genres')}: `}
-              </Typography>
-              <Typography>
-                {details?.genres?.join(', ') || t('common::unknown')}
-              </Typography>
-            </Box>
-            <Box mt={1}>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`${t('page.search.item.userScore')}: `}
-              </Typography>
-              <Box
-                display="flex"
-                alignItems="center"
-              >
-                <Rating
-                  name="userScore"
-                  size="small"
-                  defaultValue={details?.vote || 0}
-                  precision={0.1}
-                  max={10}
-                  emptyIcon={<StarBorderTwoToneIcon fontSize="inherit" />}
-                  readOnly
-                />
-                <Box ml={1}>
-                  <Typography component="span">
-                    {details?.vote || ''}
+              {PARSABLE_ITEM_TYPES.includes(details?.type) && (
+                <Box mt={1}>
+                  <Typography color="textSecondary" variant="subtitle2">
+                    {`${t(`page.search.item.${details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy'}`)}: `}
+                  </Typography>
+                  <Typography>
+                    {details?.[details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy']?.join(', ') || t('common::unknown')}
                   </Typography>
                 </Box>
+              )}
+              <Box mt={1}>
+                <Typography color="textSecondary" variant="subtitle2">
+                  {`${t('page.search.item.genres')}: `}
+                </Typography>
+                <Typography>
+                  {details?.genres?.join(', ') || t('common::unknown')}
+                </Typography>
               </Box>
-            </Box>
-            <Box mt={1}>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`${t('page.search.item.homepage')}: `}
-              </Typography>
-              <Typography>
-                {details?.homepage ? (
-                  <Link
-                    href={details.homepage}
-                    target="_blank"
-                    color="secondary"
-                    className={classes.homepageLink}
-                  >
-                    {details.homepage}
-                  </Link>
-                ) : (
-                  t('common::unknown')
-                )}
-              </Typography>
-            </Box>
+              <Box mt={1}>
+                <Typography color="textSecondary" variant="subtitle2">
+                  {`${t('page.search.item.userScore')}: `}
+                </Typography>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                >
+                  <Rating
+                    name="userScore"
+                    size="small"
+                    defaultValue={details?.vote || 0}
+                    precision={0.1}
+                    max={10}
+                    emptyIcon={<StarBorderTwoToneIcon fontSize="inherit" />}
+                    readOnly
+                  />
+                  <Box ml={1}>
+                    <Typography component="span">
+                      {details?.vote || ''}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Box mt={1}>
+                <Typography color="textSecondary" variant="subtitle2">
+                  {`${t('page.search.item.homepage')}: `}
+                </Typography>
+                <Typography>
+                  {details?.homepage ? (
+                    <Link
+                      href={details.homepage}
+                      target="_blank"
+                      color="secondary"
+                      className={classes.homepageLink}
+                    >
+                      {details.homepage}
+                    </Link>
+                  ) : (
+                    t('common::unknown')
+                  )}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" variant="subtitle2">
+              {`${t('page.search.item.status')}: `}
+            </Typography>
+            <Typography>
+              {details?.status || t('common::unknown')}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" variant="subtitle2">
+              {`${t('page.search.item.numberOfSeasons')}: `}
+            </Typography>
+            <Typography>
+              {details?.numberOfSeasons || t('common::unknown')}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" variant="subtitle2">
+              {`${t('page.search.item.numberOfEpisodes')}: `}
+            </Typography>
+            <Typography>
+              {details?.numberOfEpisodes || t('common::unknown')}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography color="textSecondary" variant="subtitle2">
+              {`${t('page.search.item.epidoseRunTimes')}: `}
+            </Typography>
+            <Typography>
+              {details?.epidoseRunTimes?.[0] || t('common::unknown')}
+            </Typography>
           </Grid>
           <Grid item xs={12}>
             <Typography color="textSecondary" variant="subtitle2">
