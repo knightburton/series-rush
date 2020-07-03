476e9625360381fa6953dd321e1c3f885d9c908b
Imre Kiss <jakab.asd@gmail.com>
2020-07-03 16:11:00 +0200

    use the new item-details widget to render the details dialog content #71

--- src/components/pages/search/search-result-list-item-details-dialog/index.jsx
+++ src/components/pages/search/search-result-list-item-details-dialog/index.jsx
@@ -10,27 +10,15 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Typography from '@material-ui/core/Typography';
-import Hidden from '@material-ui/core/Hidden';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Rating from '@material-ui/lab/Rating';
-
-import StarBorderTwoToneIcon from '@material-ui/icons/StarBorderTwoTone';
 
 import FormButton from '../../../commons/form-button';
+import ItemDetails from '../../../widgets/item-details';
 
 import {
   getSerachResultDetailsDialogOpen,
   getSearchResultDetails,
   closeSearchResultDetailsDialog,
 } from '../../../../store/search';
-import {
-  ITEM_TYPES,
-  PARSABLE_ITEM_TYPES,
-} from '../../../../constants/config';
 
 import useStyles from './styles';
 
@@ -70,180 +58,10 @@ const SearchResultListItemDetailsDialog = () => {
         </Box>
       </DialogTitle>
       <DialogContent>
-        <Grid container spacing={2}>
-          <Grid container item spacing={2} xs={12}>
-            <Grid item xs="auto">
-              <Hidden xsDown>
-                <img
-                  src={details?.posterPath}
-                  alt={details?.name}
-                  draggable={false}
-                  className={classes.poster}
-                />
-              </Hidden>
-              <Hidden smUp>
-                <img
-                  src={details?.backdropPath}
-                  alt={details?.name}
-                  draggable={false}
-                  className={classes.backdrop}
-                />
-              </Hidden>
-            </Grid>
-            <Grid item>
-              <Box>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.premiere')}: `}
-                </Typography>
-                <Typography>
-                  {details?.premiere || t('common::unknown')}
-                </Typography>
-              </Box>
-              {PARSABLE_ITEM_TYPES.includes(details?.type) && (
-                <Box mt={1}>
-                  <Typography color="textSecondary" variant="subtitle2">
-                    {`${t(`page.search.item.${details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy'}`)}: `}
-                  </Typography>
-                  <Typography>
-                    {details?.[details.type === ITEM_TYPES.TV ? 'createdBy' : 'directedBy']?.join(', ') || t('common::unknown')}
-                  </Typography>
-                </Box>
-              )}
-              <Box mt={1}>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.genres')}: `}
-                </Typography>
-                <Typography>
-                  {details?.genres?.join(', ') || t('common::unknown')}
-                </Typography>
-              </Box>
-              <Box mt={1}>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.userScore')}: `}
-                </Typography>
-                <Box
-                  display="flex"
-                  alignItems="center"
-                >
-                  <Rating
-                    name="userScore"
-                    size="small"
-                    defaultValue={details?.vote || 0}
-                    precision={0.1}
-                    max={10}
-                    emptyIcon={<StarBorderTwoToneIcon fontSize="inherit" />}
-                    readOnly
-                  />
-                  <Box ml={1}>
-                    <Typography component="span">
-                      {details?.vote || ''}
-                    </Typography>
-                  </Box>
-                </Box>
-              </Box>
-              <Box mt={1}>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.homepage')}: `}
-                </Typography>
-                <Typography>
-                  {details?.homepage ? (
-                    <Link
-                      href={details.homepage}
-                      target="_blank"
-                      color="secondary"
-                      className={classes.homepageLink}
-                    >
-                      {details.homepage}
-                    </Link>
-                  ) : (
-                    t('common::unknown')
-                  )}
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-          {details.type === ITEM_TYPES.TV && (
-            <>
-              <Grid item>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.status')}: `}
-                </Typography>
-                <Typography>
-                  {details?.status || t('common::unknown')}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.numberOfSeasons')}: `}
-                </Typography>
-                <Typography>
-                  {details?.numberOfSeasons || t('common::unknown')}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.numberOfEpisodes')}: `}
-                </Typography>
-                <Typography>
-                  {details?.numberOfEpisodes || t('common::unknown')}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography color="textSecondary" variant="subtitle2">
-                  {`${t('page.search.item.epidoseRunTimes')}: `}
-                </Typography>
-                <Typography>
-                  {details?.epidoseRunTimes?.[0] || t('common::unknown')}
-                </Typography>
-              </Grid>
-            </>
-          )}
-          <Grid item xs={12}>
-            <Typography color="textSecondary" variant="subtitle2">
-              {`${t('page.search.item.overview')}: `}
-            </Typography>
-            <Typography>
-              {details?.overview || t('common::unknown')}
-            </Typography>
-          </Grid>
-          {details.type === ITEM_TYPES.TV && (
-            <Grid item xs={12}>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`${t('page.search.item.seasons')}: `}
-              </Typography>
-              {details?.seasons?.length > 0 ? (
-                details.seasons.map((season, index) => (
-                  <Box mb={1} key={season.index}>
-                    <Card variant="outlined">
-                      <CardContent>
-                        <Typography color="textSecondary">
-                          {season?.name || index + 1}
-                          {` (${t('page.search.item.episodes')}: ${season?.numberOfEpisodes || 0})`}
-                        </Typography>
-                        {season?.date && (
-                          <Typography>
-                            {season.date}
-                          </Typography>
-                        )}
-                        {season?.overview && (
-                          <Box mt={1}>
-                            <Typography>
-                              {season.overview}
-                            </Typography>
-                          </Box>
-                        )}
-                      </CardContent>
-                    </Card>
-                  </Box>
-                ))
-              ) : (
-                <Typography>
-                  {t('common::unknown')}
-                </Typography>
-              )}
-            </Grid>
-          )}
-        </Grid>
+        <ItemDetails
+          details={details}
+          disableName
+        />
       </DialogContent>
       <DialogActions>
         <FormButton
