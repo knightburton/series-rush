b1ade9d1717ceb3723f5baef1e089548ab5b0aee
Imre Kiss <jakab.asd@gmail.com>
2021-12-10 18:50:03 +0100

    Restructure the source folder

---
+++ src/components/App.tsx
@@ -0,0 +1,3 @@
+const App = (): JSX.Element => <main>Series-Rush</main>;
+
+export default App;
--- src/contexts/customTheme.tsx
+++ src/contexts/customTheme.tsx
@@ -2,9 +2,13 @@ import React, { useState, useMemo, useCallback } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { PaletteMode, Theme } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import localStorage from '../side-effects/local-storage';
+import localStorage from '../sideEffects/localStorage';
 import { CustomThemeContextInterface, CustomThemeProviderProps } from '../interfaces';
 import { LOCAL_STORAGE_KEYS } from '../constants/core';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const CustomThemeContext = React.createContext<CustomThemeContextInterface>({
   colorMode: 'light',
@@ -13,11 +17,13 @@ const CustomThemeContext = React.createContext<CustomThemeContextInterface>({
 
 export const CustomThemeProvider = ({ children }: CustomThemeProviderProps): JSX.Element => {
   const [mode, setMode] = useState<PaletteMode>(localStorage.get(LOCAL_STORAGE_KEYS.CUSTOM_THEME, 'light'));
+
   const toggleColorMode = useCallback<() => void>(() => {
     const newMode = mode === 'light' ? 'dark' : 'light';
     localStorage.set(LOCAL_STORAGE_KEYS.CUSTOM_THEME, newMode);
     setMode(newMode);
   }, [mode]);
+
   const value = useMemo<CustomThemeContextInterface>(() => ({ colorMode: mode, toggleColorMode }), [mode, toggleColorMode]);
   const theme = useMemo<Theme>(() => createTheme({ palette: { mode } }), [mode]);
 
