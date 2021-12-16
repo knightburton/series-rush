915017620ac53318693fc36d60eefd50b5a4c2ce
Imre Kiss <jakab.asd@gmail.com>
2021-12-16 22:26:39 +0100

    Add Button as a core component

---
+++ src/components/core/Button/index.tsx
@@ -0,0 +1,8 @@
+import MuiButton from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+const Button = styled(MuiButton)({
+  textTransform: 'none',
+});
+
+export default Button;
