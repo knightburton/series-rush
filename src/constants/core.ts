65fae6215165bf7e046646116fcf29d84c3162c8
Imre Kiss <jakab.asd@gmail.com>
2021-12-28 14:52:03 +0100

    Define tge firebase config constants and fill up from .env

--- src/constants/core.ts
+++ src/constants/core.ts
@@ -2,6 +2,18 @@ export const LOCAL_STORAGE_KEYS = {
   CUSTOM_THEME: 'CUSTOM_THEME',
 };
 
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
 export default {
   LOCAL_STORAGE_KEYS,
+  FIREBASE_CONFIG,
 };
