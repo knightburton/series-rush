dbc3f091481762e67d74481a4bdef1b7474c3623
Imre Kiss <jakab.asd@gmail.com>
2021-12-28 14:50:24 +0100

    Add utils for select and omit object keys

---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+const filterObject = (object: object, filter: (key: string) => boolean): object =>
+  Object.fromEntries(Object.entries(object).filter(([key]) => filter(key.toLowerCase())));
+
+export const selectObject = (object: object, keys: string[]): object => {
+  const lowProps = keys.map(key => key.toLowerCase());
+  return filterObject(object, key => lowProps.includes(key));
+};
+
+export const omitObject = (object: object, keys: string[]): object => {
+  const lowProps = keys.map(key => key.toLowerCase());
+  return filterObject(object, key => !lowProps.includes(key));
+};
