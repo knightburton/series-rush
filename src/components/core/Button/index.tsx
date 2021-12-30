2bd9dcb024b1ca56301eecdbdbb2f3cf801b46d1
Imre Kiss <jakab.asd@gmail.com>
2021-12-30 17:08:17 +0100

    Extend the Button component with loading indicator #100

--- src/components/core/Button/index.tsx
+++ src/components/core/Button/index.tsx
@@ -1,8 +1,29 @@
 import MuiButton from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import { ButtonProps as MuiButtonProps } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
-const Button = styled(MuiButton)({
-  textTransform: 'none',
-});
+export interface ButtonProps extends MuiButtonProps {
+  loading?: boolean;
+}
+
+const Button = ({ loading, sx, children, ...rest }: ButtonProps): JSX.Element => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <MuiButton {...rest} sx={{ position: 'relative', textTransform: 'none', '&,&.Mui-disabled': { color: loading ? 'transparent' : undefined }, ...sx }}>
+    {children}
+    {loading && (
+      <CircularProgress
+        size={24}
+        sx={{
+          color: 'secondary.main',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          marginTop: '-12px',
+          marginLeft: '-12px',
+        }}
+      />
+    )}
+  </MuiButton>
+);
 
 export default Button;
