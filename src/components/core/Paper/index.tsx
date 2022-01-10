df3ffa12c0ce4b9074230671f8e755d9833461e1
Imre Kiss <jakab.asd@gmail.com>
2022-01-10 18:32:18 +0100

    Add custom box shadow to the paper core component

--- src/components/core/Paper/index.tsx
+++ src/components/core/Paper/index.tsx
@@ -1,8 +1,14 @@
 import MuiPaper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import { PaperProps } from '@mui/material';
 
-const Paper = styled(MuiPaper)(({ theme }) => ({
-  padding: theme.spacing(1),
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Paper = styled((props: PaperProps) => <MuiPaper variant="elevation" elevation={0} {...props} />)(({ theme }) => ({
+  padding: theme.spacing(2),
+  boxShadow:
+    theme.palette.mode === 'light'
+      ? 'rgb(145 158 171 / 20%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px'
+      : 'rgb(0 0 0 / 20%) 0px 0px 2px 0px, rgb(0 0 0 / 12%) 0px 12px 24px -4px',
 }));
 
 export default Paper;
