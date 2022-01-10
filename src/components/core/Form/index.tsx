1093e1f908054bada7cfecff69bfb5e0359388cc
Imre Kiss <jakab.asd@gmail.com>
2022-01-10 18:32:37 +0100

    Fix the margin on the core Form component

--- src/components/core/Form/index.tsx
+++ src/components/core/Form/index.tsx
@@ -2,12 +2,12 @@ import { memo } from 'react';
 import Box from '@mui/material/Box';
 
 export interface FormProps {
-  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
   children?: React.ReactNode;
 }
 
 const Form = ({ onSubmit, children }: FormProps): JSX.Element => (
-  <Box sx={{ width: 1, my: 2 }}>
+  <Box sx={{ width: 1 }}>
     <form onSubmit={onSubmit} noValidate>
       {children}
     </form>
