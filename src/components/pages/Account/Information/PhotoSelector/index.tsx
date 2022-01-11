33e868f6289a66e2afaf80068f484fd4f7669cc4
Imre Kiss <jakab.asd@gmail.com>
2022-01-11 22:51:29 +0100

    Move the account photo selector into a separated component #103

---
+++ src/components/pages/Account/Information/PhotoSelector/index.tsx
@@ -0,0 +1,104 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Input from '@mui/material/Input';
+import NoPhotographyOutlinedIcon from '@mui/icons-material/NoPhotographyOutlined';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import { useSelector } from '../../../../../hooks/redux';
+import { getUserAvatar } from '../../../../../store/auth';
+
+const PhotoSelector = (): JSX.Element => {
+  const { t } = useTranslation();
+  const [hover, setHover] = useState<boolean>(true);
+  const [photo, setPhoto] = useState<File>();
+  const fileInput = useRef<HTMLInputElement | null>(null);
+  const avatar = useSelector(getUserAvatar);
+
+  const handleAddClick = useCallback<() => void>(() => {
+    fileInput.current?.click();
+  }, []);
+  const handleInputChange = useCallback<(event: React.ChangeEvent<HTMLInputElement>) => void>(event => {
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) {
+      const file = input.files[0];
+      setPhoto(file);
+    }
+  }, []);
+
+  return (
+    <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+      <Box
+        sx={({ spacing }) => ({
+          my: 1,
+          p: 0.5,
+          position: 'relative',
+          width: spacing(16),
+          height: spacing(16),
+          border: 1,
+          borderRadius: '50%',
+          borderStyle: 'dashed',
+          borderColor: 'text.secondary',
+          cursor: hover ? 'pointer' : 'auto',
+          overflow: 'hidden',
+        })}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={handleAddClick}
+      >
+        {avatar && !photo && <img src={avatar} alt="User avatar" style={{ width: '100%', height: '100%', borderRadius: '50%' }} />}
+        {photo && <img src={URL.createObjectURL(photo)} alt="User temporary avatar" style={{ width: '100%', height: '100%', borderRadius: '50%' }} />}
+        {!avatar && !photo && (
+          <Box
+            sx={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              bgcolor: 'action.disabled',
+              borderRadius: '50%',
+            }}
+          >
+            <NoPhotographyOutlinedIcon color="disabled" />
+            <Typography variant="caption" color="text.secondary">
+              {t('account.imageMissing')}
+            </Typography>
+          </Box>
+        )}
+        {hover && (
+          <Box
+            sx={({ spacing }) => ({
+              position: 'absolute',
+              top: spacing(0.5),
+              left: spacing(0.5),
+              width: `calc(100% - ${spacing(1)})`,
+              height: `calc(100% - ${spacing(1)})`,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              bgcolor: 'background.default',
+              opacity: 0.65,
+              borderRadius: '50%',
+            })}
+          >
+            <AddAPhotoIcon />
+            <Typography variant="caption">{t('account.imageAdd')}</Typography>
+          </Box>
+        )}
+      </Box>
+      <Typography variant="caption" color="text.secondary">
+        {t('account.imageTypes')}
+      </Typography>
+      <Typography variant="caption" color="text.secondary">
+        {t('account.imageSize')}
+      </Typography>
+      <Input inputProps={{ ref: fileInput, accept: '.jpeg,.jpg,.png,.gif' }} type="file" sx={{ display: 'none' }} onChange={handleInputChange} />
+    </Stack>
+  );
+};
+
+export default PhotoSelector;
--- src/components/pages/Account/Information/index.tsx
+++ src/components/pages/Account/Information/index.tsx
@@ -1,10 +1,6 @@
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import NoPhotographyOutlinedIcon from '@mui/icons-material/NoPhotographyOutlined';
 import useForm, { Schema } from '@knightburton/react-use-form';
 import Paper from '../../../core/Paper';
 import Form from '../../../core/Form';
@@ -12,6 +8,7 @@ import FormText from '../../../core/FormText';
 import ButtonContainer from '../../../core/ButtonContainer';
 import Button from '../../../core/Button';
 import Confirmation from '../../../core/Confirmation';
+import PhotoSelector from './PhotoSelector';
 import { TEXT_MIN, TEXT_MAX, EMAIL } from '../../../../constants/validation';
 
 export interface InformationForm {
@@ -68,44 +65,7 @@ const Information = (): JSX.Element => {
         <Grid container spacing={2}>
           <Grid item xs={12} md={4}>
             <Paper>
-              <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-                <Box
-                  sx={({ spacing }) => ({
-                    my: 1,
-                    p: 0.5,
-                    width: spacing(16),
-                    height: spacing(16),
-                    border: 1,
-                    borderRadius: '50%',
-                    borderStyle: 'dashed',
-                    borderColor: 'text.secondary',
-                  })}
-                >
-                  <Box
-                    sx={{
-                      width: '100%',
-                      height: '100%',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      bgcolor: 'action.disabled',
-                      borderRadius: '50%',
-                    }}
-                  >
-                    <NoPhotographyOutlinedIcon color="disabled" />
-                    <Typography variant="caption" color="text.secondary">
-                      {t('translation:account.imageMissing')}
-                    </Typography>
-                  </Box>
-                </Box>
-                <Typography variant="caption" color="text.secondary">
-                  {t('translation:account.imageTypes')}
-                </Typography>
-                <Typography variant="caption" color="text.secondary">
-                  {t('translation:account.imageSize')}
-                </Typography>
-              </Stack>
+              <PhotoSelector />
             </Paper>
           </Grid>
           <Grid item xs={12} md={8}>
