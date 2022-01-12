b8e5ff808d49adad8933ba9a07ef699ccd8d6c20
Imre Kiss <jakab.asd@gmail.com>
2022-01-12 19:01:52 +0100

    Refactor the core Title to have primary and secondary variant

--- src/components/core/Title/index.tsx
+++ src/components/core/Title/index.tsx
@@ -1,17 +1,25 @@
 import { memo } from 'react';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export interface TitleProps {
   children: string;
-  mt?: number;
-  mb?: number;
+  variant?: 'primary' | 'secondary';
 }
 
-const Title = ({ children, mt = 2, mb = 2 }: TitleProps): JSX.Element => (
-  <Box sx={{ mt, mb }}>
-    <Typography variant="h6">{children}</Typography>
-  </Box>
+const Title = ({ children, variant = 'primary' }: TitleProps): JSX.Element => (
+  <Typography
+    variant={variant === 'primary' ? 'h6' : 'caption'}
+    color={variant === 'primary' ? 'inherit' : 'text.secondary'}
+    component="p"
+    sx={{
+      mt: variant === 'primary' ? 2 : 0,
+      mb: 2,
+      textTransform: variant === 'secondary' ? 'uppercase' : 'none',
+      fontWeight: variant === 'secondary' ? 'bold' : 'normal',
+    }}
+  >
+    {children}
+  </Typography>
 );
 
 export default memo(Title);
