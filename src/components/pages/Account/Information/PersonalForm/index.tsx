bf308daa8f719c9f4ab5c9f29b95d8199f4f9832
Imre Kiss <jakab.asd@gmail.com>
2022-01-24 19:59:54 +0100

    Implement the base profile information update action calls #103

--- src/components/pages/Account/Information/PersonalForm/index.tsx
+++ src/components/pages/Account/Information/PersonalForm/index.tsx
@@ -1,33 +1,24 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import useForm, { Schema } from '@knightburton/react-use-form';
+import Stack from '@mui/material/Stack';
 import Form from '../../../../core/Form';
 import FormText from '../../../../core/FormText';
-import ButtonContainer from '../../../../core/ButtonContainer';
 import Button from '../../../../core/Button';
 import Confirmation from '../../../../core/Confirmation';
+import { useSelector, useDispatch } from '../../../../../hooks/redux';
+import { getUser, getInProgressByType, updateProfileBase, User, ProgressTypes } from '../../../../../store/auth';
 import { TEXT_MIN, TEXT_MAX, EMAIL } from '../../../../../constants/validation';
 
 export interface InformationForm {
-  firstName: string;
-  lastName: string;
+  displayName: string;
   email: string;
 }
 
-const schema = (t: (key: string, options?: object) => string): Schema<InformationForm> => [
+const schema = (t: (key: string, options?: object) => string, user: User | null): Schema<InformationForm> => [
   {
-    field: 'firstName',
-    value: '',
-    required: true,
-    requiredError: t('error:required'),
-    validators: [
-      { rule: TEXT_MIN(3), error: t('error:textMin', { min: 3 }) },
-      { rule: TEXT_MAX(255), error: t('error:textMax', { max: 255 }) },
-    ],
-  },
-  {
-    field: 'lastName',
-    value: '',
+    field: 'displayName',
+    value: user?.displayName || '',
     required: true,
     requiredError: t('error:required'),
     validators: [
@@ -37,7 +28,7 @@ const schema = (t: (key: string, options?: object) => string): Schema<Informatio
   },
   {
     field: 'email',
-    value: '',
+    value: user?.email || '',
     required: true,
     requiredError: t('error:required'),
     validators: [{ rule: EMAIL, error: t('error:email') }],
@@ -47,33 +38,65 @@ const schema = (t: (key: string, options?: object) => string): Schema<Informatio
 const PersonalForm = () => {
   const { t } = useTranslation(['common', 'translation', 'error']);
   const [open, setOpen] = useState<boolean>(false);
+  const [formHasChnaged, setFormHasChanged] = useState<boolean>(false);
+  const [data, setData] = useState<InformationForm>({ displayName: '', email: '' });
+  const dispatch = useDispatch();
+  const user = useSelector<User | null>(getUser);
+  const inProgress = useSelector<boolean>(getInProgressByType(ProgressTypes.BaseUpdate));
 
-  const onSubmit = useCallback<() => void>(() => setOpen(true), []);
-  const handleAgree = useCallback<() => void>(() => setOpen(false), []);
+  const onSubmit = useCallback<(validatedData: InformationForm) => void>(validatedData => {
+    setData(validatedData);
+    setOpen(true);
+  }, []);
+  const handleAgree = useCallback<() => void>(() => {
+    setOpen(false);
+    dispatch(updateProfileBase(data));
+    setData({ displayName: '', email: '' });
+  }, [data, dispatch]);
   const handleDisagree = useCallback<() => void>(() => setOpen(false), []);
-  const { fields, handleSubmit, handleChange } = useForm<InformationForm>({
-    schema: schema(t),
+  const { fields, handleSubmit, handleChange, updateSchema } = useForm<InformationForm>({
+    schema: schema(t, user),
     onSubmit,
   });
+  const handleCancelClick = useCallback(() => {
+    updateSchema(schema(t, user));
+  }, [updateSchema, t, user]);
+
+  useEffect(() => {
+    setFormHasChanged(fields.displayName.value !== user?.displayName || fields.email.value !== user?.email);
+  }, [fields, user]);
 
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormText
-          id="firstName"
-          label={t('common:firstName')}
+          id="displayName"
+          label={t('common:displayName')}
+          onChange={handleChange}
+          value={fields.displayName.value}
+          error={fields.displayName.error}
+          autoComplete="displayName"
+          disabled={inProgress}
+        />
+        <FormText
+          id="email"
+          label={t('common:email')}
           onChange={handleChange}
           value={fields.email.value}
           error={fields.email.error}
-          autoComplete="firstName"
+          autoComplete="email"
+          disabled={inProgress}
         />
-        <FormText id="lastName" label={t('common:lastName')} onChange={handleChange} value={fields.email.value} error={fields.email.error} autoComplete="lastName" />
-        <FormText id="email" label={t('common:email')} onChange={handleChange} value={fields.email.value} error={fields.email.error} autoComplete="email" />
-        <ButtonContainer align="flex-end">
-          <Button onClick={() => handleSubmit()} variant="contained">
+        <Stack direction="row" justifyContent="flex-end" spacing={2} sx={{ mt: 2 }}>
+          {formHasChnaged && (
+            <Button onClick={handleCancelClick} variant="contained" color="warning" disabled={inProgress}>
+              {t('translation:account.cancelEdit')}
+            </Button>
+          )}
+          <Button type="submit" variant="contained" disabled={!formHasChnaged} loading={inProgress}>
             {t('translation:account.save')}
           </Button>
-        </ButtonContainer>
+        </Stack>
       </Form>
       <Confirmation
         id="update-account"
