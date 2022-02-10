073dde41d1d2b1cf17ab3e915010226052f6b404
Imre Kiss <jakab.asd@gmail.com>
2022-02-10 22:41:48 +0100

    Create a new header for Profile page #112

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,13 +1,56 @@
-import { useTranslation } from 'react-i18next';
 import Container from '@mui/material/Container';
-import Title from 'components/core/Title';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import NoPhotographyOutlinedIcon from '@mui/icons-material/NoPhotographyOutlined';
+import Paper from 'components/core/Paper';
+import { useSelector } from 'hooks/redux';
+import { getUserDisplayName, getUserProfilePhoto } from 'store/auth';
 
 const Profile = () => {
-  const { t } = useTranslation();
+  const profilePhoto = useSelector<string>(getUserProfilePhoto);
+  const displayName = useSelector<string>(getUserDisplayName);
 
   return (
     <Container maxWidth="lg">
-      <Title>{t('profile.title')}</Title>
+      <Paper sx={{ position: 'relative', pl: 20, mt: 6 }}>
+        <Box
+          sx={({ spacing }) => ({
+            position: 'absolute',
+            top: spacing(-4),
+            left: spacing(2),
+            p: 0.5,
+            width: spacing(16),
+            height: spacing(16),
+            border: 1,
+            borderRadius: '50%',
+            borderStyle: 'dashed',
+            borderColor: 'text.secondary',
+            bgcolor: 'transparent',
+            overflow: 'hidden',
+          })}
+        >
+          {profilePhoto ? (
+            <img src={profilePhoto} alt="User avatar" style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
+          ) : (
+            <Box
+              sx={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '50%',
+              }}
+            >
+              <NoPhotographyOutlinedIcon color="disabled" />
+            </Box>
+          )}
+        </Box>
+        <Typography color="text.secondary" variant="h6">
+          {displayName}
+        </Typography>
+      </Paper>
     </Container>
   );
 };
