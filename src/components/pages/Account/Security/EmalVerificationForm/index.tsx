411da26fef6f0f10edc77ab7e693d51e29a81d3d
Imre Kiss <jakab.asd@gmail.com>
2022-02-11 23:50:21 +0100

    Change the icons on the EmailVerificationForm #109

--- src/components/pages/Account/Security/EmalVerificationForm/index.tsx
+++ src/components/pages/Account/Security/EmalVerificationForm/index.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import MarkEmailReadOutlinedIcon from '@mui/icons-material/MarkEmailReadOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
 import Paper from 'components/core/Paper';
 import Title from 'components/core/Title';
 import { useSelector } from 'hooks/redux';
@@ -16,7 +16,7 @@ const EmailVerificationForm = (): JSX.Element => {
     <Paper>
       <Title variant="secondary">{t('account.emailVerification')}</Title>
       <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-        {verified ? <MarkEmailReadOutlinedIcon sx={{ fontSize: 100, color: 'primary.main' }} /> : <EmailOutlinedIcon sx={{ fontSize: 100, color: 'error.main' }} />}
+        {verified ? <CheckOutlinedIcon sx={{ fontSize: 100, color: 'primary.main' }} /> : <CloseOutlinedIcon sx={{ fontSize: 100, color: 'error.main' }} />}
         <Typography color="text.secondary">{t('account.emailVerified', { verified })}</Typography>
       </Stack>
     </Paper>
